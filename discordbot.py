46bd1ff8e0689e9d2d9362366a46d0b71474da30
steve323team <130263583+steve323team@users.noreply.github.com>
2023-04-16 22:01:22 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,30 +1,27 @@
-from cmath import log
-from distutils.sysconfig import PREFIX
 import discord
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-PREFIX = os.environ['PREFIX']
-TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
-
+    print('login')
+    print(client.user.name)
+    print(client.user.id)
+    print('-----------------')
+    await client.change_presence(game=discord.Game(name='DeNi BOT', type=1))
+    
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
-
+    if message.channel.is_private and message.author.id != "1082274007648317452":
+     await client.send_message(discord.utils.get(client.get_all_members(), id="1063636047436730378"), message.author.name + "(" + message.author.id + ") : " + message.content)
+    
+       if client.private_channels and message.author.id != "1082274007648317452":
+        await client.send_message(client.get_channel("1097156613544091729"), message.author.name + "(" + message/author.id + ") : " + message.content)
+        
+       if message.content.startswith("!DM"):
+        member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
+        await client.send.message(member, "DeNi : " + message.content[23:])
+        
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
