0594a0bc12e87cd3b11c85d7cb937467cf5d6a62
steve323team <130263583+steve323team@users.noreply.github.com>
2023-04-16 22:32:48 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,7 +20,7 @@ async def on_message(message):
         await client.send_message(discord.utils.get(client.get_all_members(), id="1063636047436730378"), message.author.name + "(" + message.author.id + ") : " + message.content)
     
        if message.channel.is_private and message.author.id != "1082274007648317452":
-        await client.send_message(client.get_channel("1097156613544091729"), message.author.name + "(" + message/author.id + ") : " + message.content)
+        await client.send_message(client.get_channel("1097156613544091729"), message.author.name + "(" + message.author.id + ") : " + message.content)
         
        if message.content.startswith("!DM"):
         member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
