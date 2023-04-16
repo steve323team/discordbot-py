f53b187c7dbd808fa68485c5416521328670c2bb
steve323team <130263583+steve323team@users.noreply.github.com>
2023-04-16 22:45:17 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,26 +3,16 @@ import discord
 
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('login')
-    print(client.user.name)
-    print(client.user.id)
-    print('-----------------')
-    
 @client.event
 async def on_message(message):
-       if message.channel.is_private and message.author.id != "1082274007648317452":
-        await client.send_message(discord.utils.get(client.get_all_members(), id="1063636047436730378"), message.author.name + "(" + message.author.id + ") : " + message.content)
-    
-       if message.channel.is_private and message.author.id != "1082274007648317452":
-        await client.send_message(client.get_channel("1097156613544091729"), message.author.name + "(" + message.author.id + ") : " + message.content)
-        
-       if message.content.startswith("!DM"):
-        member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
-        await client.send.message(member, "DeNi : " + message.content[23:])
+    if client.private_channels and message.author.id != ("1082274007648317452"):
+        await client.get_channel("1097156613544091729").send(message.author.name + "(" + str(message.author.id) + "): " + message.content)
+
+
+    if message.content.startswith("!DM"):
+        author = message.guild.get_member(int(message.content[4:22]))
+        msg = message.content[23:]
+        await author.send(msg)
         
 try:
     client.run(TOKEN)
